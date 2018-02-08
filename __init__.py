92503e42956d6d5e6bb3596b5795154a013f18dc
Alexey-t <support@uvviewsoft.com>
2018-02-08 13:41:05 +0300

    trim space

--- __init__.py
+++ __init__.py
@@ -14,11 +14,11 @@ def load_font(fontpath):
 class Command:
     def on_start(self, ed_self):
         if os.name!='nt': return
-    
+
         dir = os.path.join(app_path(APP_DIR_DATA), 'fonts')
         if not os.path.isdir(dir): return
 
-        count = 0        
+        count = 0
         for name in os.listdir(dir):
             if name.endswith('.ttf') \
             or name.endswith('.ttc') \
@@ -26,5 +26,5 @@ class Command:
             or name.endswith('.fon'):
                 count += 1
                 ok = load_font(os.path.join(dir, name))
-                msg = '' if ok else '... failed' 
+                msg = '' if ok else '... failed'
                 print('Loading font:', name, msg)
