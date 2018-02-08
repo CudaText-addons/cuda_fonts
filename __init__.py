c7719cf2fb4a957dadfa93f593fd5540c24a666a
Alexey-t <support@uvviewsoft.com>
2018-02-08 13:40:21 +0300

    refac

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
 import os
-#import win32api
-#import win32con
 import ctypes
 from cudatext import *
 
-#def notify_os():
-    #win32api.SendMessage(win32con.HWND_BROADCAST, win32con.WM_FONTCHANGE)
+FR_PRIVATE  = 0x10
+FR_NOT_ENUM = 0x20
 
 def load_font(fontpath):
-    FR_PRIVATE  = 0x10
-    FR_NOT_ENUM = 0x20
     pathbuf = ctypes.create_unicode_buffer(fontpath)
     AddFontResourceEx = ctypes.windll.gdi32.AddFontResourceExW
     num = AddFontResourceEx(ctypes.byref(pathbuf), FR_PRIVATE, 0)
@@ -21,13 +17,14 @@ class Command:
     
         dir = os.path.join(app_path(APP_DIR_DATA), 'fonts')
         if not os.path.isdir(dir): return
-        
-        names = os.listdir(dir)
-        for name in names:
+
+        count = 0        
+        for name in os.listdir(dir):
             if name.endswith('.ttf') \
             or name.endswith('.ttc') \
             or name.endswith('.otf') \
             or name.endswith('.fon'):
+                count += 1
                 ok = load_font(os.path.join(dir, name))
                 msg = '' if ok else '... failed' 
                 print('Loading font:', name, msg)
